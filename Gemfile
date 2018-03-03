4bdd610019e7ecfc6a1aff1b4d7f5cb7cd5d2b64
Mike <mdatko@sbcglobal.net>
2018-03-03 02:51:43 +0000

    added bootstrap styling

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ end
 gem 'rails', '~> 5.1.5'
 gem 'devise'
 gem 'milia'
+gem 'twitter-bootstrap-rails'
+gem 'devise-bootstrap-views'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_tenant!
+ # before_action :authenticate_tenant!
   protect_from_forgery with: :exception
   before_action :authenticate_tenant!
   
